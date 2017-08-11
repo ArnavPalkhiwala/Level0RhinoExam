4fcea5a152da850e4c403f54a0428b002766c24d
Steven Liu <stevotennis@gmail.com>
2017-08-10 23:57:27 -0700

    Exam Question

---
+++ src/CodingExercise1.java
@@ -0,0 +1,25 @@
+/* Level 0 Exam: Coding  Exercise #1 */
+
+/**
+ * Write a program that asks the user for their name. 
+ * Get a random number and print out their name random number
+ * amount of times. When the loop is running at an odd number
+ * print out that user is weird.
+ **/
+
+/*
+ * Example:
+ * Name: Esteban
+ * Random number: 3
+ * 
+ * output:
+ * Esteban is odd.
+ * Esteban
+ * Esteban is odd. 
+ **/
+
+import javax.swing.JOptionPane;
+
+public class CodingExercise1 {
+
+}
